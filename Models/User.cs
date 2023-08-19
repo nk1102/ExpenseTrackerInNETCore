4b8e474648d920f0e3bc74e88bc5790a63de348f
Nikunj <nikunjkhandekwal1234@gmail.com>
2023-08-19 18:47:46 +0530

    Added Model Users for Creation of User and Admin for the Website

---
+++ Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTrackerInNETCore.Models
+{
+    public class User
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Name Cannot be Blank")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email Cannot be Blank")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password Cannot be Blank")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "MobileNumber Cannot be Blank")]
+        public long MobileNumber { get; set; }
+
+        public bool IsAdmin { get; set; } = false;
+
+        public bool IsEmailVerified { get; set; } = false;
+
+
+
+    }
+}
