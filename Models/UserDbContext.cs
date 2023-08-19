1ec2ad0f6b40dc30f4887ed49cf6c6df8616b525
Nikunj <nikunjkhandekwal1234@gmail.com>
2023-08-19 18:50:29 +0530

    Added User DbContext.cs For trying to authenticate the Users to database

---
+++ Models/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTrackerInNETCore.Models
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<User> Users { get; set; }
+    }
+}
