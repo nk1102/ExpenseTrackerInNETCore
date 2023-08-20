6765be1fd0a04b41aa6274fe9c7389a2950b6a5d
Nikunj <nikunjkhandekwal1234@gmail.com>
2023-08-20 13:32:47 +0530

    Register page has been made for the Website

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,5 +23,14 @@ namespace ExpenseTrackerInNETCore.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register ()
+        {
+            return View();
+        }
+
     }
 }
